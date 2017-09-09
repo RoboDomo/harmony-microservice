1521c5efa3e95f97f1b7959c8850dd6a24fb9f52
Michael Schwartz <mykesx@gmail.com>
2017-09-08 20:55:32 -0700

    fixed startng activity bug

--- index.js
+++ index.js
@@ -27,137 +27,138 @@ const mqttHost  = process.env.MQTT_HOST || 'mqtt://ha',
 const POLL_TIME = 100
 
 function getCommandsFromControlGroup(controlGroup) {
-    const deviceCommands = {}
+  const deviceCommands = {}
 
-    controlGroup.some(function (group) {
-        group.function.some(function (func) {
-            slug                 = parameterize(func.label)
-            deviceCommands[slug] = {name: func.name, slug: slug, label: func.label}
-            Object.defineProperty(deviceCommands[slug], "action", {
-                enumerable: false,
-                writeable:  true,
-                value:      func.action.replace(/\:/g, '::')
-            });
-        })
+  controlGroup.some(function (group) {
+    group.function.some(function (func) {
+      const slug  = parameterize(func.label)
+
+      deviceCommands[slug] = {name: func.name, slug: slug, label: func.label}
+      Object.defineProperty(deviceCommands[slug], 'action', {
+        enumerable: false,
+        writeable:  true,
+        value:      func.action.replace(/:/g, '::')
+      })
     })
-    return deviceCommands
+  })
+  return deviceCommands
 }
 
 class HarmonyHost extends HostBase {
-    constructor(config) {
-        super(mqttHost, topicRoot + '/' + config.device, true)
-        debug('construct', config)
-        this.device = config.device
-        this.ip     = config.ip
-        this.mac    = config.mac
-        this.state = { startingActivity: null}
-        this.client.on('connect', () => {
-            this.client.subscribe(this.topic + '/set/#')
+  constructor(config) {
+    super(mqttHost, topicRoot + '/' + config.device, true)
+    debug('construct', config)
+    this.device = config.device
+    this.ip     = config.ip
+    this.mac    = config.mac
+    this.state = { startingActivity: null}
+    this.client.on('connect', () => {
+      this.client.subscribe(this.topic + '/set/#')
+    })
+    this.client.on('message', async (topic, message) => {
+      message = message.toString()
+      debug(this.topic, 'topic', topic, 'message', message.substr(0, 32))
+      if (topic.endsWith('command')) {
+        return Promise.resolve(await this.command(message))
+      }
+      else if (topic.endsWith('activity')) {
+        return Promise.resolve(await this.startActivity(message))
+      }
+      const parts = topic.split('/')
+      if (parts[3] === 'device') {
+        this.deviceCommand(parts[4], message)
+
+      }
+      else {
+        debug('invalid topic', topic)
+      }
+    })
+  }
+
+  async connect() {
+    return new Promise(async (resolve, reject) => {
+      debug(this.device, 'connecting')
+      try {
+        this.harmonyClient = await harmony(this.ip)
+        process.on('exit', () => {
+          debug(this.device, 'exit', 'harmonyClient.end()')
+          this.harmonyClient.end()
         })
-        this.client.on('message', async (topic, message) => {
-            message = message.toString()
-            debug(this.topic, 'topic', topic, 'message', message.substr(0, 32))
-            if (topic.endsWith('command')) {
-                return Promise.resolve(await this.command(message))
-            }
-            else if (topic.endsWith('activity')) {
-                return Promise.resolve(await this.startActivity(message))
-            }
-            const parts = topic.split('/')
-            if (parts[3] === 'device') {
-                this.deviceCommand(parts[4], message)
-
-            }
-            else {
-                debug('invalid topic', topic)
-            }
+        process.on('SIGINT', () => {
+          debug(this.device, 'SIGINT', 'harmonyClient.end()')
+          this.harmonyClient.end()
+          process.exit()
         })
-    }
-
-    async connect() {
-        return new Promise(async (resolve, reject) => {
-            debug(this.device, 'connecting')
-            try {
-                this.harmonyClient = await harmony(this.ip)
-                process.on('exit', () => {
-                    debug(this.device, 'exit', 'harmonyClient.end()')
-                    this.harmonyClient.end()
-                })
-                process.on('SIGINT', () => {
-                    debug(this.device, 'SIGINT', 'harmonyClient.end()')
-                    this.harmonyClient.end()
-                    process.exit()
-                })
-                await this.refresh()
-                this.state = {
-                    availableCommands: this.availableCommands,
-                    activities:        this.activities,
-                    devices:           this.devices
-                }
-                this.poll()
-                resolve()
-            }
-            catch (e) {
-                debug(this.harmonyClient, 'exception', e)
-                reject(e)
-            }
-        })
-    }
-
-    async refresh() {
-        return new Promise(async (resolve, reject) => {
-            try {
-                this.availableCommands = await this.harmonyClient.getAvailableCommands()
-                this.devices           = await this.getDevices()
-                this.activities        = await this.getActivities()
-                // TODO: activities fixit?
-                resolve()
-            }
-            catch (e) {
-                reject(e)
-            }
-        })
-    }
-
-    getCommands() {
-        const currentActivity = this.state.activities[this.state.currentActivity],
-              controlGroup    = currentActivity.controlGroup,
-              commands        = {}
-
-        controlGroup.forEach((group) => {
-            group.function.forEach((func) => {
-                commands[func.name] = func
-            })
-        })
-        return commands
-    }
-
-    findActivity(activity) {
-        debug('findActivity', activity)
-        try {
-            const activities = this.activities
-            if (!this.activities) {
-                return null
-            }
-
-            if (activities[activity] || activities[String(activity)]) {
-                return activity
-            }
-            for (const key of Object.keys(activities)) {
-                const a = activities[key]
-                console.log('compare', key, a.label)
-                if (a.label === activity) {
-                    return key
-                }
-            }
-            return null
+        await this.refresh()
+        this.state = {
+          availableCommands: this.availableCommands,
+          activities:        this.activities,
+          devices:           this.devices
         }
-        catch (e) {
-            return null
-        }
-    }
+        this.poll()
+        resolve()
+      }
+      catch (e) {
+        debug(this.harmonyClient, 'exception', e)
+        reject(e)
+      }
+    })
+  }
 
-    /**
+  async refresh() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        this.availableCommands = await this.harmonyClient.getAvailableCommands()
+        this.devices           = await this.getDevices()
+        this.activities        = await this.getActivities()
+        // TODO: activities fixit?
+        resolve()
+      }
+      catch (e) {
+        reject(e)
+      }
+    })
+  }
+
+  getCommands() {
+    const currentActivity = this.state.activities[this.state.currentActivity],
+          controlGroup    = currentActivity.controlGroup,
+          commands        = {}
+
+    controlGroup.forEach((group) => {
+      group.function.forEach((func) => {
+        commands[func.name] = func
+      })
+    })
+    return commands
+  }
+
+  findActivity(activity) {
+    debug('findActivity', activity)
+    try {
+      const activities = this.activities
+      if (!this.activities) {
+        return null
+      }
+
+      if (activities[activity] || activities[String(activity)]) {
+        return activity
+      }
+      for (const key of Object.keys(activities)) {
+        const a = activities[key]
+        console.log('compare', key, a.label)
+        if (a.label === activity) {
+          return key
+        }
+      }
+      return null
+    }
+    catch (e) {
+      return null
+    }
+  }
+
+  /**
      * Start an activity.
      *
      * @param activity - activty ID of actifity to start.  PowerOff ('-1') will
@@ -165,207 +166,207 @@ class HarmonyHost extends HostBase {
      *
      * @returns {Promise.<Promise|*|Q.Promise>}
      */
-    async startActivity(activity) {
-        console.log('activity', activity)
-        activity = this.findActivity(activity)
-        if (!activity) {
-            return
-        }
-        console.log('activity after', activity)
-        return new Promise(async (resolve, reject) => {
-            try {
-                this.state = {startingActivity: activity}
-                this.publish()
-                const ret  = await this.harmonyClient.startActivity(activity)
-                this.state = {startingActivity: null}
-                this.publish()
-                resolve(ret)
-            }
-            catch (e) {
-                debug('exception ', e)
-                reject(e)
-            }
-        })
+  async startActivity(activity) {
+    console.log('activity', activity)
+    activity = this.findActivity(activity)
+    if (!activity) {
+      return
     }
+    console.log('activity after', activity)
+    return new Promise(async (resolve, reject) => {
+      try {
+        this.state = {startingActivity: activity}
+        this.publish()
+        const ret  = await this.harmonyClient.startActivity(activity)
+        this.state = {startingActivity: null}
+        this.publish()
+        resolve(ret)
+      }
+      catch (e) {
+        debug('exception ', e)
+        reject(e)
+      }
+    })
+  }
 
-    async poll() {
-        debug(this.device, 'poll')
-        while (1) {
-            const startingActivity = this.state ? this.state.startingActivity : null,
-                  currentActivity  = startingActivity || await this.getCurrentActivity(),
-                  newActivity = this.state.currentActivity !== currentActivity
+  async poll() {
+    debug(this.device, 'poll')
+    while (true) {
+      const startingActivity = this.state ? this.state.startingActivity : null,
+            currentActivity  = await this.getCurrentActivity(),
+            newActivity = this.state.currentActivity !== currentActivity
 
-            try {
-                this.state = {
-                    isOff:             await this.harmonyClient.isOff(),
-                    currentActivity:   currentActivity,
-                    startingActivity: startingActivity === currentActivity ? null : startingActivity,
-                    // availableCommands: this.availableCommands,
-                    // activities:        this.activities,
-                    // devices:           this.devices
-                }
-                if (newActivity) {
-                    this.state = {
-                        commands: this.getCommands()    // get commands for current activity
-                    }
-                }
-                // if (!this.once) {
-                //     debug(this.device, this.state)
-                //     this.once = true
-                // }
-            }
-            catch (e) {
-                debug(this.device, 'poll exception', e)
-            }
-            await this.wait(POLL_TIME)
+      try {
+        this.state = {
+          isOff:            await this.harmonyClient.isOff(),
+          currentActivity:  currentActivity,
+          startingActivity: startingActivity === currentActivity ? null : startingActivity,
+          // availableCommands: this.availableCommands,
+          // activities:        this.activities,
+          // devices:           this.devices
         }
+        if (newActivity) {
+          this.state = {
+            commands: this.getCommands()    // get commands for current activity
+          }
+        }
+        // if (!this.once) {
+        //     debug(this.device, this.state)
+        //     this.once = true
+        // }
+      }
+      catch (e) {
+        debug(this.device, 'poll exception', e)
+      }
+      await this.wait(POLL_TIME)
     }
+  }
 
-    /**
+  /**
      * @private
      * @returns {Promise.<Promise|*|Q.Promise>}
      */
-    async getCurrentActivity() {
-        return this.harmonyClient.getCurrentActivity()
-    }
+  async getCurrentActivity() {
+    return this.harmonyClient.getCurrentActivity()
+  }
 
-    /**
+  /**
      * @private
      * @returns {Promise.<{}>}
      */
-    async getActivities() {
-        const activities = {}
+  async getActivities() {
+    const activities = {}
 
-        try {
-            const records = await this.harmonyClient.getActivities()
-            // debug(this.device, 'getActivities', records)
-            records.forEach((activity) => {
-                activities[activity.id] = activity
-            })
-            return activities
-        }
-        catch (e) {
-            debug(this.device, 'updateActivities exception', e)
-            throw e
-        }
+    try {
+      const records = await this.harmonyClient.getActivities()
+      // debug(this.device, 'getActivities', records)
+      records.forEach((activity) => {
+        activities[activity.id] = activity
+      })
+      return activities
     }
+    catch (e) {
+      debug(this.device, 'updateActivities exception', e)
+      throw e
+    }
+  }
 
-    /**
+  /**
      * @private
      * @returns {Promise.<{}>}
      */
-    async getDevices() {
-        const devices = {},
-              slugs   = {}
-        try {
-            const commands = await this.harmonyClient.getAvailableCommands()
-            // debug(this.device, 'getAvailableCommands', commands)
-            commands.device.forEach((device) => {
-                device.slug        = parameterize(device.label)
-                device.commands    = getCommandsFromControlGroup(device.controlGroup)
-                devices[device.id] = device
-                slugs[device.slug] = device
-            })
-            this.deviceSlugs = slugs
-            return devices
-        }
-        catch (e) {
-            debug(this.device, 'updateDevices exception', e)
-            throw e
-        }
+  async getDevices() {
+    const devices = {},
+          slugs   = {}
+    try {
+      const commands = await this.harmonyClient.getAvailableCommands()
+      // debug(this.device, 'getAvailableCommands', commands)
+      commands.device.forEach((device) => {
+        device.slug        = parameterize(device.label)
+        device.commands    = getCommandsFromControlGroup(device.controlGroup)
+        devices[device.id] = device
+        slugs[device.slug] = device
+      })
+      this.deviceSlugs = slugs
+      return devices
     }
+    catch (e) {
+      debug(this.device, 'updateDevices exception', e)
+      throw e
+    }
+  }
 
-    /**
+  /**
      * Execute a command, simulating a button press/release
      * @param command
      * @returns {Promise.<*>}
      */
-    async command(command) {
-        debug(this.device, 'command', command)
-        const commands = this.state.commands,
-              control  = commands ? commands[command] : null
+  async command(command) {
+    debug(this.device, 'command', command)
+    const commands = this.state.commands,
+          control  = commands ? commands[command] : null
 
-        if (!control) {
-            debug(this.device, 'command', command, control)
-            return Promise.resolve(new Error('Invalid command ' + command))
-        }
-
-        return new Promise(async (resolve, reject) => {
-            const action = control.action.replace(/\:/g, '::')
-            // debug(this.device, 'command', command, 'control', control, 'action', action)
-            try {
-                await this.harmonyClient.send('holdAction', 'action=' + action + ':status=press')
-                await this.harmonyClient.send('holdAction', 'action=' + action + ':status=release')
-                resolve()
-            }
-            catch (e) {
-                reject(e)
-            }
-        })
+    if (!control) {
+      debug(this.device, 'command', command, control)
+      return Promise.resolve(new Error('Invalid command ' + command))
     }
 
-    findAction(device, slug) {
-        if (!device) {
-            return null
-        }
-        const slugs = device.commands
+    return new Promise(async (resolve, reject) => {
+      const action = control.action.replace(/\:/g, '::')
+      // debug(this.device, 'command', command, 'control', control, 'action', action)
+      try {
+        await this.harmonyClient.send('holdAction', 'action=' + action + ':status=press')
+        await this.harmonyClient.send('holdAction', 'action=' + action + ':status=release')
+        resolve()
+      }
+      catch (e) {
+        reject(e)
+      }
+    })
+  }
 
-        if (slugs[slug]) {
-            return slugs[slug].action
-        }
+  findAction(device, slug) {
+    if (!device) {
+      return null
+    }
+    const slugs = device.commands
 
-        const keys = Object.keys(slugs)
-        let action = null
-        keys.some((key) => {
-            if (slugs[key].name === slug) {
-                action = slugs[key].action
-                return true
-            }
-        })
-        return action
+    if (slugs[slug]) {
+      return slugs[slug].action
     }
 
-    async deviceCommand(deviceSlug, command) {
-        const device = this.deviceSlugs[deviceSlug],
-              action = this.findAction(device, command)
+    const keys = Object.keys(slugs)
+    let action = null
+    keys.some((key) => {
+      if (slugs[key].name === slug) {
+        action = slugs[key].action
+        return true
+      }
+    })
+    return action
+  }
 
-        if (!action) {
-            Promise.reject(new Error('No such action ' + command))
-        }
-        if (!device) {
-            Promise.reject(new Error('No such device ' + deviceSlug))
-        }
+  async deviceCommand(deviceSlug, command) {
+    const device = this.deviceSlugs[deviceSlug],
+          action = this.findAction(device, command)
 
-        return new Promise(async (resolve, reject) => {
-            // debug(this.device, 'deviceCommand', deviceSlug, command, action)
-            try {
-                await this.harmonyClient.send('holdAction', 'action=' + action + ':status=press')
-                await this.harmonyClient.send('holdAction', 'action=' + action + ':status=release')
-                resolve()
-            }
-            catch (e) {
-                reject(e)
-            }
-        })
+    if (!action) {
+      Promise.reject(new Error('No such action ' + command))
     }
+    if (!device) {
+      Promise.reject(new Error('No such device ' + deviceSlug))
+    }
+
+    return new Promise(async (resolve, reject) => {
+      // debug(this.device, 'deviceCommand', deviceSlug, command, action)
+      try {
+        await this.harmonyClient.send('holdAction', 'action=' + action + ':status=press')
+        await this.harmonyClient.send('holdAction', 'action=' + action + ':status=release')
+        resolve()
+      }
+      catch (e) {
+        reject(e)
+      }
+    })
+  }
 }
 
 const hubs = {}
 
 async function main() {
-    Config.hubs.forEach((hub) => {
-        hubs[hub.device] = new HarmonyHost(hub)
-    })
+  Config.hubs.forEach((hub) => {
+    hubs[hub.device] = new HarmonyHost(hub)
+  })
 
-    Object.keys(hubs).forEach(async (hub) => {
-        try {
-            await hubs[hub].connect()
-        }
-        catch (e) {
-            console.dir(hub)
-            console.dir(e)
-        }
-    })
+  Object.keys(hubs).forEach(async (hub) => {
+    try {
+      await hubs[hub].connect()
+    }
+    catch (e) {
+      console.dir(hub)
+      console.dir(e)
+    }
+  })
 }
 
 main()
