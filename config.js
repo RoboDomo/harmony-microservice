5ee5b0ddfa31c0de070e769f2f6d42d27a627a83
Michael Schwartz <mykesx@gmail.com>
2017-09-24 15:11:02 -0700

    comment out 2nd hub

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // This is an example config
 // add your hubs accordingly
 module.exports = {
-    hubs: [
-        {device: 'harmony-hub', name: 'Family Room', ip: '192.168.4.14', mac: '00:04:20:f2:43:91'},
-        {device: 'harmony-hub2', name: 'Master Bedroom', ip: '192.168.4.35', mac: '00:04:20:ff:24:a7'}
-    ]
+  hubs: [
+    {device: 'harmony-hub', name: 'Family Room', ip: '192.168.4.14', mac: '00:04:20:f2:43:91'},
+    //        {device: 'harmony-hub2', name: 'Master Bedroom', ip: '192.168.4.35', mac: '00:04:20:ff:24:a7'}
+  ]
 }
